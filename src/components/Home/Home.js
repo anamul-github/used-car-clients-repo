8a10b14b163972d0da5bb1524be2871f881e9fd5
Anamul10 <anamul.hasan@mnsu.edu>
2022-11-23 16:13:01 -0500

    banner added to the home page, image hosted in img bb

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>This is Home</h2>
+            <img className='' src="https://i.ibb.co/9cR8Ryv/2.jpg" alt="car-banner" />
+
+            <div>
+                <h2>Advertised Items</h2>
+            </div>
+            <div>
+                <h2>Used Cars</h2>
+            </div>
         </div>
     );
 };
