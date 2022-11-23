656ade4fd7e67d9e1c3f819c6bb0394d0b964865
Anamul10 <anamul.hasan@mnsu.edu>
2022-11-23 13:55:39 -0500

    auth setup done, blog page done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blog = () => {
     return (
         <div className="accordion" id="accordionExample">
-            <div className="accordion-item bg-white border border-gray-200">
+            <div className="accordion-item border border-gray-200">
                 <h2 className="accordion-header mb-0" id="headingOne">
                     <button className="
         accordion-button
@@ -21,22 +21,21 @@ const Blog = () => {
         focus:outline-none
       " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
                         aria-controls="collapseOne">
-                        Accordion Item #1
+                        #1 What are the different ways to manage a state in a React application?
                     </button>
                 </h2>
                 <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne"
                     data-bs-parent="#accordionExample">
                     <div className="accordion-body py-4 px-5">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default,
-                        until the collapse plugin adds the appropriate classNamees that we use to style each
-                        element. These classNamees control the overall appearance, as well as the showing and
-                        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                        our default variables. It's also worth noting that just about any HTML can go within
-                        the <code>.accordion-body</code>, though the transition does limit overflow.
+                        <strong>The built-in way that React provides for setting component states is by using setState()</strong> and adding “local state” to a class. There are several other ways to manage state​s in React, including the use of:
+                        <br />
+                        1. Hooks <br />
+                        2. React Context API <br />
+                        3. Apollo Link State
                     </div>
                 </div>
             </div>
-            <div className="accordion-item bg-white border border-gray-200">
+            <div className="accordion-item border border-gray-200">
                 <h2 className="accordion-header mb-0" id="headingTwo">
                     <button className="
         accordion-button
@@ -55,22 +54,18 @@ const Blog = () => {
         focus:outline-none
       " type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false"
                         aria-controls="collapseTwo">
-                        Accordion Item #2
+                        #2 How does prototypical inheritance work?
                     </button>
                 </h2>
                 <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo"
                     data-bs-parent="#accordionExample">
                     <div className="accordion-body py-4 px-5">
-                        <strong>This is the second item's accordion body.</strong> It is hidden by default,
-                        until the collapse plugin adds the appropriate classNamees that we use to style each
-                        element. These classNamees control the overall appearance, as well as the showing and
-                        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                        our default variables. It's also worth noting that just about any HTML can go within
-                        the <code>.accordion-body</code>, though the transition does limit overflow.
+                        <strong>Prototypical inheritance refers to the ability to access object properties from another object.</strong>
+                        The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object.
                     </div>
                 </div>
             </div>
-            <div className="accordion-item bg-white border border-gray-200">
+            <div className="accordion-item border border-gray-200">
                 <h2 className="accordion-header mb-0" id="headingThree">
                     <button className="
         accordion-button
@@ -89,21 +84,51 @@ const Blog = () => {
         focus:outline-none
       " type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false"
                         aria-controls="collapseThree">
-                        Accordion Item #3
+                        #3 What is a unit test? Why should we write unit tests?
                     </button>
                 </h2>
                 <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree"
                     data-bs-parent="#accordionExample">
                     <div className="accordion-body py-4 px-5">
-                        <strong>This is the third item's accordion body.</strong> It is hidden by default,
-                        until the collapse plugin adds the appropriate classNamees that we use to style each
-                        element. These classNamees control the overall appearance, as well as the showing and
-                        hiding via CSS transitions. You can modify any of this with custom CSS or overriding
-                        our default variables. It's also worth noting that just about any HTML can go within
-                        the <code>.accordion-body</code>, though the transition does limit overflow.
+                        <strong>A unit test</strong> is a way of testing a unit - the smallest piece of code that can be logically isolated in a system. In most programming languages, that is a function, a subroutine, a method or property.
+                        <br /> <br />
+                        Unit testing ensures that all code meets quality standards before it's deployed. This ensures a reliable engineering environment where quality is paramount. Over the course of the product development life cycle, unit testing saves time and money, and helps developers write better code, more efficiently.
                     </div>
                 </div>
             </div>
+
+            <div className="accordion-item border border-gray-200">
+                <h2 className="accordion-header mb-0" id="headingFour">
+                    <button className="
+        accordion-button
+        collapsed
+        relative
+        flex
+        items-center
+        w-full
+        py-4
+        px-5
+        text-base text-gray-800 text-left
+        bg-white
+        border-0
+        rounded-none
+        transition
+        focus:outline-none
+      " type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false"
+                        aria-controls="collapseFour">
+                        #4 Differences between React vs. Angular vs. Vue?
+                    </button>
+                </h2>
+                <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour"
+                    data-bs-parent="#accordionExample">
+                    <div className="accordion-body py-4 px-5">
+                        <strong>Both - Angular JS and React JS frameworks are used to create web interfaces for front end development.</strong> Angular is Google's matured and advanced JavaScript framework based on TypeScript, whereas Vue is a progressive open-source front-end JavaScript framework created by Evan You.
+                        <br /> <br />
+                        It's easier to learn Vue than angular and it reasonably takes the same amount of time and effort as learning react. Although some people argue that it's even easier to learn than react but that's of course subjective and varies from person to person.
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
