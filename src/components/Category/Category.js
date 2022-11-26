6995284daba65545e0c3a85345121743ee3836a9
Anamul10 <anamul.hasan@mnsu.edu>
2022-11-25 23:01:43 -0500

    design part done for category page

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -14,19 +14,12 @@ const Category = () => {
                     <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
                         <div className="rounded overflow-hidden shadow-lg">
                             <img className="w-full" src={detail.picture} alt="Mountain" />
-                            <h2 className='text-2xl'> {detail.name}</h2>
-                            <h2>Location: {detail.location}</h2>
-                            <h2>Price: ${detail.resale_price}</h2>
-                            <h2>Original Price: ${detail.original_price}</h2>
-                            <div className="px-6 py-4">
-                                <div className="font-bold text-xl mb-2">Mountain</div>
-                                <p className="text-gray-700 text-base">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                                </p>
-                            </div>
-                            <div className="px-6 pt-4 pb-2">
-
-                            </div>
+                            <h2 className='text-2xl px-3 py-2'> {detail.name}</h2>
+                            <h2 className='px-3 py-1'>Location: {detail.location}</h2>
+                            <h2 className='text-primary px-3 py-1'>Price: ${detail.resale_price}</h2>
+                            <h2 className='px-3 py-1'>Original Price: ${detail.original_price}</h2>
+                            <h2 className='px-3 py-1'>Years of use: {detail.years_of_use} years</h2>
+                            <h2 className='px-3 py-1 pb-5'>Posted on: {detail.posted_time}</h2>
                         </div>
                     </div>
                 )
