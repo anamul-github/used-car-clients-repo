daf65d75a7f5bfa2ca13f8afea2bc6d956cdff7a
Anamul10 <anamul.hasan@mnsu.edu>
2022-11-28 01:40:18 -0500

    used get method to show all sellers to UI for admin

--- src/components/Dashboard/AllSellers/AllSellers.js
+++ src/components/Dashboard/AllSellers/AllSellers.js
@@ -1,9 +1,43 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllSellers = () => {
+    const { data: sellers = [] } = useQuery({
+        queryKey: ['sellers'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/allsellers');
+            const data = await res.json();
+            return data;
+        }
+    })
     return (
         <div>
+            <h2 className='text-3xl m-5 px-5'>All Sellers</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
 
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Admin</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            sellers.map((seller, i) => <tr key={seller._id}>
+                                <th>{i + 1}</th>
+                                <td>{seller.name}</td>
+                                <td>{seller.email}</td>
+                                <td>Blue</td>
+                                <td>Blue</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
